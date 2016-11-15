b007d79225443eb1d12ba9127ac714f2079d09f7
HristiyanZahariev <ihzahariev@gmail.com>
2016-11-16 00:19:02 +0200

    Working on the front-end Cant display entered form

--- rest/src/main/webapp/scripts.js
+++ rest/src/main/webapp/scripts.js
@@ -19,6 +19,44 @@ function getData() {
 	});
 }
 
+function formToJSON() {
+	return JSON.stringify({
+		"manufacture": $('#manufacture').val(),
+		"model": $('#model').val(),
+		"year": $('#year').val(),
+		"color": $('#color').val(),
+		});
+}
+
+function addCar() {
+	var form = $('.form-horizontal');
+	$(form).submit(function(event) {
+		// Stop the browser from submitting the form.
+		event.preventDefault();
+		var formData = formToJSON();
+		$.ajax({
+			type: 'POST',
+			contentType: 'application/json',
+			url: "http://localhost:8080/rest/api/cars",
+			dataType: "json",
+			data: formToJSON(),
+			success: function(data) {
+				console.log(data);
+				$.each(data, function(index){
+					var tr = $('<tr>');
+					tr.append('<td>' + data[index].id + '</td>');
+					tr.append('<td>' + data[index].manufacture + '</td>');
+					tr.append('<td>' + data[index].model + '</td>');
+					tr.append('<td>' + data[index].year + '</td>');
+					tr.append('<td>' + data[index].color + '</td>');
+					tr.append('</tr>');
+					$('#carsTable').append(tr);
+				});
+			}
+		});
+	});
+}
+
 $(document).ready(function() {
 		getData();
 });
